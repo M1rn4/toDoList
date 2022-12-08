5957ced3654fa3443fbfee4d816c77cabb99bc9a
mirnamp <ampueromirna8@gmail.com>
2022-12-08 10:51:02 -0500

    add addList and removeONe

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const addTask = document.querySelector('.addTask');
 const newDest = document.querySelector('.newToDo');
 const addBtn = document.querySelector('.add_button');
 const remBtn = document.querySelector('.remove_button');
-const task = [];
+let task = [];
 
 const showList = (array) => {
   // eslint-disable-next-line no-plusplus
@@ -19,7 +19,7 @@ const showList = (array) => {
             class="checkbox" 
             name="checkbox" 
             id="checkbox" ></li>
-          <li class="task-description">${array.description}</li>
+          <li class="task-description" id="${array.index + 1}">${array.description}</li>
         </ul>
       </div>
 
@@ -31,21 +31,53 @@ const showList = (array) => {
       `;
   return container;
 };
-
+window.addEventListener('load', () => {
+  const tasks = JSON.parse(localStorage.getItem('tasks'));
+  // eslint-disable-next-line no-console
+  tasks.forEach((element) => {
+    const content = showList(element);
+    addTask.innerHTML += content;
+  });
+});
 const addlist = (array) => {
   const taskN = {};
-  taskN.description = newDest.value;
-  taskN.completed = array;
+  taskN.description = array;
+  taskN.completed = false;
   taskN.index = task.length + 1;
   const listItem = showList(taskN);
   newDest.insertAdjacentHTML('beforeend', listItem);
   task.push(taskN);
-  addTask.innerHTML += listItem;
+  localStorage.setItem('tasks', JSON.stringify(task));
+  const tasks = JSON.parse(localStorage.getItem('tasks'));
+  tasks.forEach((e) => {
+    addTask.innerHTML += showList(e);
+  });
+};
+// eslint-disable-next-line no-unused-vars
+const deleteOne = (array) => {
+  let taskName = array.children[0].children[0].children[1].id;
+  // eslint-disable-next-line no-const-assign
+  taskName = parseInt(taskName, 10);
+  const tasks = JSON.parse(localStorage.getItem('tasks'));
+  // eslint-disable-next-line no-const-assign, eqeqeq
+  const ele = tasks.findIndex((e) => e.index == taskName);
+  // eslint-disable-next-line no-console
+  tasks.splice(ele - 1, 1);
+  // eslint-disable-next-line no-console
+  localStorage.setItem('tasks', JSON.stringify(tasks));
+  // eslint-disable-next-line no-console
+  addTask.innerHTML = '';
+  tasks.forEach((e) => {
+    // eslint-disable-next-line no-unused-expressions    
+    addTask.innerHTML += showList(e);
+  });
+  console.log(tasks);
 };
 
 function addBtnN() {
   // eslint-disable-next-line no-console
-  addlist(false);
+  addlist(newDest.value);
+  newDest.value = '';
 }
 function removeAll() {
   // eslint-disable-next-line no-const-assign
@@ -55,5 +87,20 @@ function removeAll() {
 }
 
 remBtn.addEventListener('click', removeAll);
-
 addBtn.addEventListener('click', addBtnN);
+addTask.addEventListener('click', (e) => {
+  const task = e.target.parentElement.parentElement;
+  if (e.target.classList.contains('del')) {
+    // eslint-disable-next-line no-console
+    deleteOne(task, e.target);
+  }
+  // eslint-disable-next-line max-len
+  // // task will be edited when first the input field of task is updated and then edit icon is clicked
+  // if (e.target.classList.contains('edit')) {
+  //   editTask(task);
+  // }
+  // if (e.target.classList.contains('checkbox')) {
+  //   const tasks = JSON.parse(localStorage.getItem('tasks'));
+  //   updateTaskStatus(e.target, tasks);
+  // }
+});
