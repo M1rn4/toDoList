0f6fd01f14c5170e00e8fed850cb2b2633708750
mirnamp <ampueromirna8@gmail.com>
2022-12-10 08:20:32 -0500

    change name

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: './dist/index.js',
   mode: 'development',
   output: {
     filename: 'main.js',
